046d342bba6c4f35d08c42d47cc70b0bd210fee6
yakupdurmus <yakup.durmus@justlife.com>
2025-02-01 15:01:45 +0300

    Start android emulator

--- extension.js
+++ extension.js
@@ -1,78 +1,129 @@
-const vscode = require('vscode');
-const { exec } = require('child_process');
+const vscode = require("vscode");
+const { exec } = require("child_process");
+
+const emulatorPath = "~/Library/Android/sdk/emulator/emulator";
 
 function activate(context) {
+  console.log(
+    'Congratulations, your extension "justlife-vs-extension" is now active!'
+  );
 
-	console.log('Congratulations, your extension "justlife-vs-extension" is now active!');
+  const disposableHello = vscode.commands.registerCommand(
+    "justlife-vs-extension.helloWorld",
+    function () {
+      vscode.window.showInformationMessage(
+        "Hello World from justlife-vs-extension!"
+      );
+    }
+  );
 
-	const disposableHello = vscode.commands.registerCommand('justlife-vs-extension.helloWorld', function () {
-		vscode.window.showInformationMessage('Hello World from justlife-vs-extension!');
-	});
+  const disposableSimulator = vscode.commands.registerCommand(
+    "justlife-vs-extension.simulator",
+    async function () {
+      const simulatorOptions = ["Android", "iOS"];
+      const selectedSimulator = await vscode.window.showQuickPick(
+        simulatorOptions,
+        {
+          placeHolder: "Select a simulator",
+        }
+      );
 
-	const disposableSimulator = vscode.commands.registerCommand('justlife-vs-extension.simulator', async function () {
-		const simulatorOptions = ['Android', 'iOS'];
-		const selectedSimulator = await vscode.window.showQuickPick(simulatorOptions, {
-			placeHolder: 'Select a simulator'
-		});
+      if (selectedSimulator) {
+        if (selectedSimulator === "iOS") {
+          exec(
+            'xcrun simctl list devices available | grep -E "iPhone|iPad"',
+            (error, stdout, stderr) => {
+              if (error) {
+                vscode.window.showErrorMessage(`Error: ${stderr}`);
+                return;
+              }
+              const iosOptions = stdout.split("\n").filter((line) => line);
+              console.log(iosOptions);
+              showIOSOptions(iosOptions);
+            }
+          );
+        } else if (selectedSimulator === "Android") {
+          exec(`${emulatorPath} -list-avds`, (error, stdout, stderr) => {
+            if (error) {
+              vscode.window.showErrorMessage(`Error: ${stderr}`);
+              return;
+            }
+            const androidOptions = stdout.split("\n").filter((line) => line);
+            showAndroidOptions(androidOptions);
+          });
+        }
+      }
+    }
+  );
 
-		if (selectedSimulator) {
-			if (selectedSimulator === 'iOS') {
-				exec('xcrun simctl list devices available | grep -E "iPhone|iPad"', (error, stdout, stderr) => {
-					if (error) {
-						vscode.window.showErrorMessage(`Error: ${stderr}`);
-						return;
-					}
-					const iosOptions = stdout.split('\n').filter(line => line);
-					console.log(iosOptions);
-					showIOSOptions(iosOptions);
-				});
-			} else if (selectedSimulator === 'Android') {
-				const androidOptions = ['Android Emulator 1', 'Android Emulator 2']; // Buraya Android simülatörlerini ekleyin
-				const selectedAndroid = await vscode.window.showQuickPick(androidOptions, {
-					placeHolder: 'Select an Android simulator'
-				});
-				if (selectedAndroid) {
-					vscode.window.showInformationMessage(`Selected Android Emulator: ${selectedAndroid}`);
-				}
-			}
-		}
-	});
-
-	context.subscriptions.push(disposableHello);
-	context.subscriptions.push(disposableSimulator);
+  context.subscriptions.push(disposableHello);
+  context.subscriptions.push(disposableSimulator);
 }
 
 function showIOSOptions(iosOptions) {
-	vscode.window.showQuickPick(iosOptions, {
-		placeHolder: 'Select an iOS simulator'
-	}).then(selectedIOS => {
-		if (selectedIOS) {
-			const simulatorId = selectedIOS.match(/\(([^)]+)\)/)[1]; // Seçilen simülatörün ID'sini alıyoruz
-			console.log("Simulator ID ", simulatorId);
-			startIOSSimulator(simulatorId);
-		}
-	});
+  vscode.window
+    .showQuickPick(iosOptions, {
+      placeHolder: "Select an iOS simulator",
+    })
+    .then((selectedIOS) => {
+      if (selectedIOS) {
+        const simulatorId = selectedIOS.match(/\(([^)]+)\)/)[1]; // Seçilen simülatörün ID'sini alıyoruz
+        console.log("Simulator ID ", simulatorId);
+        startIOSSimulator(simulatorId);
+      }
+    });
+}
+
+// Android simülatörlerini gösteren fonksiyon
+function showAndroidOptions(androidOptions) {
+  vscode.window
+    .showQuickPick(androidOptions, {
+      placeHolder: "Select an Android emulator",
+    })
+    .then((selectedAndroid) => {
+      if (selectedAndroid) {
+        startAndroidSimulator(selectedAndroid); // Seçilen Android simülatörünü başlatıyoruz
+      }
+    });
+}
+
+function startAndroidSimulator(emulatorName) {
+  exec(`${emulatorPath} -avd ${emulatorName}`, (error, stdout, stderr) => {
+    if (error) {
+      vscode.window.showErrorMessage(
+        `Error starting Android emulator: ${stderr}`
+      );
+      return;
+    }
+    vscode.window.showInformationMessage(
+      `Android Emulator started: ${emulatorName}`
+    );
+  });
 }
 
 function startIOSSimulator(simulatorId) {
-	exec(`xcrun simctl boot ${simulatorId}`, (error, stdout, stderr) => {
-		if (error) {
-			vscode.window.showErrorMessage(`Error starting simulator: ${stderr}`);
-			return;
-		}
-		exec('open -a Simulator', (error) => {
-			if (error) {
-				vscode.window.showErrorMessage(`Error opening Simulator app: ${error.message}`);
-				return;
-			}
-		});
-		vscode.window.showInformationMessage(`iOS Simulator started: ${simulatorId}`);
-	});
+  exec(`xcrun simctl boot ${simulatorId}`, (error, stdout, stderr) => {
+    if (error) {
+      vscode.window.showErrorMessage(`Error starting simulator: ${stderr}`);
+      return;
+    }
+    exec("open -a Simulator", (error) => {
+      if (error) {
+        vscode.window.showErrorMessage(
+          `Error opening Simulator app: ${error.message}`
+        );
+        return;
+      }
+    });
+    vscode.window.showInformationMessage(
+      `iOS Simulator started: ${simulatorId}`
+    );
+  });
 }
 
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
